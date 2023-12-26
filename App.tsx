75c6f44f6d7999f869c436555e6083f5f59a3d9e
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-26 23:12:21 +0530

    remove unused code

--- App.tsx
+++ App.tsx
@@ -8,23 +8,14 @@ import {
   useColorScheme,
 } from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
 import Signup from './components/Signup';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView>
+      <StatusBar />
       <Signup />
     </SafeAreaView>
   );
