73f6ae6a7ab2ed3916394a5412c9baac75871620
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-28 13:12:06 +0530

    Revert "made custom hooks and break components"
    This reverts commit b706ee44b70d020acb889b7dc69fe8ede26bbd0d.

--- App.tsx
+++ App.tsx
@@ -2,21 +2,45 @@ import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {useSession} from './hooks/useSession';
-import AuthNavigation from './navigation/AuthNavigation';
-import AppNavigation from './navigation/AppNavigation';
+import Signup from './screens/Signup';
+import Login from './screens/Login';
+import Dashboard from './screens/Dashboard';
+import {supabase} from './lib/supabase';
 
-function App() {
+function App(): React.JSX.Element {
   const AuthStack = createNativeStackNavigator();
   const AppStack = createNativeStackNavigator();
-  const [isLoggedIn, setIsLoggedIn] = useSession();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const checkSession = async () => {
+      const {data, error} = await supabase.auth.getSession();
+      if (error) {
+        console.error('Error fetching session:', error);
+        return;
+      }
+      setIsLoggedIn(true);
+    };
+
+    checkSession();
+  }, []);
   return (
     <NavigationContainer>
       {!isLoggedIn ? (
-        <AuthNavigation setIsLoggedIn={setIsLoggedIn} />
+        <AuthStack.Navigator initialRouteName="Sign up">
+          <AuthStack.Screen name="Sign up">
+            {props => <Signup {...props} setIsLoggedIn={setIsLoggedIn} />}
+          </AuthStack.Screen>
+          <AuthStack.Screen name="Login">
+            {props => <Login {...props} setIsLoggedIn={setIsLoggedIn} />}
+          </AuthStack.Screen>
+        </AuthStack.Navigator>
       ) : (
-        <AppNavigation setIsLoggedIn={setIsLoggedIn} />
+        <AppStack.Navigator initialRouteName="Dashboard">
+          <AppStack.Screen name="Dashboard">
+            {props => <Dashboard {...props} setIsLoggedIn={setIsLoggedIn} />}
+          </AppStack.Screen>
+        </AppStack.Navigator>
       )}
     </NavigationContainer>
   );
